b9efd16055718ca8cf20fa4f08b26d04f1b67ee8
Pelt06 <peltzjr11@gmail.com>
2021-11-29 16:19:11 -0600

    Creating models

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Estudiante(models.Model):
+    carnet  =   models.CharField(max_length=30)
+    nombre  =   models.CharField(max_length=30)
+    apellido  =   models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+class Curso(models.Model):
+    nombre    = models.CharField(max_length=60)
+    seccion      = models.CharField(max_length=1)
+    actores   = models.ManyToManyField(Estudiante, through='Asignacion')
+
+    def __str__(self):
+        return self.nombre
+
+class Asignacion (models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+
+class AsignacionInLine(admin.TabularInline):
+    model = Asignacion
+    extra = 1
+
+
+class EstudianteAdmin(admin.ModelAdmin):
+    inlines = (AsignacionInLine,)
+
+class CursoAdmin (admin.ModelAdmin):
+    inlines = (AsignacionInLine,)
